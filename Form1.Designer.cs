d5821966a75d1628f4f5a87c77df9aa3f43d5b03
Owen Logtenberg <LogtOwen761@GoTVDSB.ca>
2021-03-16 12:52:39 -0400

    PUSHHHHH

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.imgHang = new System.Windows.Forms.PictureBox();
+            this.lblUsedLetters = new System.Windows.Forms.Label();
+            this.lstLetters = new System.Windows.Forms.ListBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.btnHard = new System.Windows.Forms.Button();
+            this.btnNormal = new System.Windows.Forms.Button();
+            this.btnEasy = new System.Windows.Forms.Button();
+            this.lblWord = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgHang)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(25, 34);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(220, 29);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Welcome to Hangman";
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(27, 75);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(308, 13);
+            this.lblInstructions.TabIndex = 1;
+            this.lblInstructions.Text = "Enter a letter to reveal the hidden word. 3 strikes and you\'re out!";
+            // 
+            // imgHang
+            // 
+            this.imgHang.Location = new System.Drawing.Point(30, 91);
+            this.imgHang.Name = "imgHang";
+            this.imgHang.Size = new System.Drawing.Size(305, 247);
+            this.imgHang.TabIndex = 2;
+            this.imgHang.TabStop = false;
+            // 
+            // lblUsedLetters
+            // 
+            this.lblUsedLetters.AutoSize = true;
+            this.lblUsedLetters.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsedLetters.Location = new System.Drawing.Point(502, 44);
+            this.lblUsedLetters.Name = "lblUsedLetters";
+            this.lblUsedLetters.Size = new System.Drawing.Size(142, 29);
+            this.lblUsedLetters.TabIndex = 3;
+            this.lblUsedLetters.Text = "Used Letters";
+            // 
+            // lstLetters
+            // 
+            this.lstLetters.FormattingEnabled = true;
+            this.lstLetters.Location = new System.Drawing.Point(507, 77);
+            this.lstLetters.Name = "lstLetters";
+            this.lstLetters.Size = new System.Drawing.Size(205, 264);
+            this.lstLetters.TabIndex = 4;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(507, 364);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 5;
+            this.txtInput.Visible = false;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Enabled = false;
+            this.btnGuess.Location = new System.Drawing.Point(630, 364);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(67, 20);
+            this.btnGuess.TabIndex = 6;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Visible = false;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // btnHard
+            // 
+            this.btnHard.Location = new System.Drawing.Point(367, 111);
+            this.btnHard.Name = "btnHard";
+            this.btnHard.Size = new System.Drawing.Size(108, 41);
+            this.btnHard.TabIndex = 7;
+            this.btnHard.Text = "Hard";
+            this.btnHard.UseVisualStyleBackColor = true;
+            // 
+            // btnNormal
+            // 
+            this.btnNormal.Location = new System.Drawing.Point(367, 61);
+            this.btnNormal.Name = "btnNormal";
+            this.btnNormal.Size = new System.Drawing.Size(108, 41);
+            this.btnNormal.TabIndex = 8;
+            this.btnNormal.Text = "Normal";
+            this.btnNormal.UseVisualStyleBackColor = true;
+            // 
+            // btnEasy
+            // 
+            this.btnEasy.Location = new System.Drawing.Point(367, 12);
+            this.btnEasy.Name = "btnEasy";
+            this.btnEasy.Size = new System.Drawing.Size(108, 41);
+            this.btnEasy.TabIndex = 9;
+            this.btnEasy.Text = "Easy";
+            this.btnEasy.UseVisualStyleBackColor = true;
+            this.btnEasy.Click += new System.EventHandler(this.btnEasy_Click);
+            // 
+            // lblWord
+            // 
+            this.lblWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWord.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.lblWord.Location = new System.Drawing.Point(12, 394);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(501, 47);
+            this.lblWord.TabIndex = 10;
+            this.lblWord.Text = "__ __ __ __ __ __ __ __ __";
+            this.lblWord.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.btnEasy);
+            this.Controls.Add(this.btnNormal);
+            this.Controls.Add(this.btnHard);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lstLetters);
+            this.Controls.Add(this.lblUsedLetters);
+            this.Controls.Add(this.imgHang);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.lblTitle);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Hangman";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgHang)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.PictureBox imgHang;
+        private System.Windows.Forms.Label lblUsedLetters;
+        private System.Windows.Forms.ListBox lstLetters;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Button btnHard;
+        private System.Windows.Forms.Button btnNormal;
+        private System.Windows.Forms.Button btnEasy;
+        private System.Windows.Forms.Label lblWord;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,79 @@ namespace Hangman_Lite
 {
     public partial class Form1 : Form
     {
+        Random generator = new Random();
+        List<string> words = new List<string>();
+        List<string> guessedletters = new List<string>();
+        int guesses = 0;
+        int difficulty = 0; // 0 = Easy
+        string guess = "";
+        string chosen = "";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnGuess.Enabled = false;
+        }
+
+        private void btnEasy_Click(object sender, EventArgs e)
+        {
+            lblWord.Text = "__ __ __ __ __";
+            words.Add("CHINA");
+            words.Add("SCALE");
+            words.Add("JOKER");
+            words.Add("ROYAL");
+            words.Add("TRASH");
+            words.Add("CHALK");
+            words.Add("ENTER");
+            words.Add("GIANT");
+            words.Add("FLOCK");
+            words.Add("APPLE");
+            words.Add("BELLY");
+            words.Add("HEAVY");
+            words.Add("INTEL");
+            words.Add("JUICE");
+            words.Add("KNOWN");
+            words.Add("LOSER");
+            words.Add("METAL");
+            words.Add("OPERA");
+            words.Add("RALLY");
+            words.Add("QUEUE");
+            words.Add("PAINT");
+            words.Add("TABLE");
+            lstLetters.DataSource = words;
+            chosen = words[generator.Next(23)];
+            lblTitle.Text = chosen;
+            txtInput.Visible = true;
+            btnGuess.Visible = true;
+
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            guess = txtInput.Text.ToUpper();
+            if (chosen.IndexOf(guess) >= 0)
+            {
+                chosen.Remove(chosen.IndexOf(guess));
+                guessedletters.Add(guess);
+                if (chosen.IndexOf(guess) == 0)
+                {
+                    if 
+                    lblWord.Text = (guess + " __ __ __ __");
+                }
+                lstLetters.DataSource = null;
+                lstLetters.DataSource = words;
+            }
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            if (txtInput.Text.Length != 1)
+                btnGuess.Enabled = false;
+            else
+                btnGuess.Enabled = true;
         }
     }
 }
